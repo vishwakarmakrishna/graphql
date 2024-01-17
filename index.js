871739af07c7fee4157606d20b882c013abcebae
krishna vishwakarma <60446420+vishwakarmakrishna@users.noreply.github.com>
2024-01-17 18:41:46 +0530

    started resolver

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ app.get("/", (req, res) => {
   res.send("GraphQL is amazing!");
 });
 
+class Product {
+  constructor(id, { name, desciption, price, soldout, stores }) {
+    this.id = id;
+    this.name = name;
+    this.desciption = desciption;
+    this.price = price;
+    this.soldout = soldout;
+    this.stores = stores;
+  }
+}
+
+const productDatabase = {};
+
 const root = {
   product: () => {
     return {
@@ -25,6 +38,11 @@ const root = {
         },
       ],
     };
+  },
+  createProduct: ({ input }) => {
+    let id = require("crypto").randomBytes(10).toString("hex");
+    productDatabase[id] = input;
+    return new Product(id, input);
   },
 };
 
--- schema.js
+++ schema.js
@@ -17,5 +17,22 @@ const schema = buildSchema(`
     type Query{
         product: Product
     }
+    
+    type StoreInput{
+        store: String
+    }
+
+    input ProductInput{
+        id: ID
+        name: String
+        description: String
+        price: Float
+        soldout: Boolean
+        stores: [StoreInput]!
+    }
+
+    type Mutation{
+        createProduct(input: ProductInput): Product
+    }
 `);
 export default schema;
